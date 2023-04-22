db8a1382d4572003f7929c4b53e56379f8813ec8
99pmore <morenomartin.pablo@gmail.com>
2023-04-22 19:48:50 +0200

    country info buttons

--- src/pages/CountryInfo.js
+++ src/pages/CountryInfo.js
@@ -2,11 +2,47 @@ import { useLocation } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import{ faLink, faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons'
+import Swal from 'sweetalert2'
 
 export const CountryInfo = () => {
 
+    const user = JSON.parse(localStorage.getItem('user'))
     const location = useLocation()
     const country = location.state.country
+
+    const handleVisitedButton = async () => {
+        await fetch(`http://localhost:4000/api/users/${user._id}/countriesVisited`, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({name: country.name})
+        })
+        .then(() => {
+            Swal.fire({
+                position: 'top-end',
+                icon: 'success',
+                title: 'Se ha añadido a tus países visitados',
+                showConfirmButton: false,
+                timer: 1500
+            })
+        })
+    }
+
+    const handleToVisitButton = async () => {
+        await fetch(`http://localhost:4000/api/users/${user._id}/countriesToVisit`, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({name: country.name})
+        })
+        .then(() => {
+            Swal.fire({
+                position: 'top-end',
+                icon: 'success',
+                title: 'Se ha añadido a tus países por visitar',
+                showConfirmButton: false,
+                timer: 1500
+            })
+        })
+    }
 
     return(
         <div className="info-container">
@@ -22,9 +58,15 @@ export const CountryInfo = () => {
                     </div>
                 </div>
                 <div className="sec-info">
-                    <h3>Capital: { country.capital }</h3>
-                    <h3>Población: { country.population } habitantes</h3>
-                    <Link className='link' to={ country.map }>Google Maps <FontAwesomeIcon icon={ faArrowUpRightFromSquare } /></Link>
+                    <div className="sec-data">
+                        <h3>Capital: { country.capital }</h3>
+                        <h3>Población: { country.population } habitantes</h3>
+                        <Link className='link' to={ country.map }>Google Maps <FontAwesomeIcon icon={ faArrowUpRightFromSquare } /></Link>
+                    </div>
+                    <div className="buttons">
+                        <button onClick={ handleVisitedButton }>Visitado</button>
+                        <button onClick={ handleToVisitButton }>Por visitar</button>
+                    </div>
                 </div>
             </div>
         </div>
